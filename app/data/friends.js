037c5b0b44970c932b83d8727ee80463b7d3e442
212680803 <randy.porter1@ge.com>
2018-12-19 11:58:16 -0500

    Uploading code

---
+++ app/data/friends.js
@@ -0,0 +1,44 @@
+var friendsArray = [
+	{
+		name: 'Perry',
+		scores: [
+			'5',
+			'1',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Marvin',
+		scores: [
+			'1',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Tony',
+		scores: [
+			'3',
+			'4',
+			'1',
+			'1',
+			'2'
+		]
+	},
+	{
+		name: 'Ganga',
+		scores: [
+			'3',
+			'5',
+			'4',
+			'2',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
